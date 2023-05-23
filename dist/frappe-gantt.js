91063261323d5866d203b0550de3267f5559edb5
Devjyoti Ghosh <devjyotighosh02@gmail.com>
2023-05-23 17:56:37 +0530

    Updated build

--- dist/frappe-gantt.js
+++ dist/frappe-gantt.js
@@ -1,6 +1,235 @@
 var Gantt = (function () {
     'use strict';
 
+    function $(expr, con) {
+        return typeof expr === 'string'
+            ? (con || document).querySelector(expr)
+            : expr || null;
+    }
+
+    function createSVG(tag, attrs) {
+        const elem = document.createElementNS('http://www.w3.org/2000/svg', tag);
+        for (let attr in attrs) {
+            if (attr === 'append_to') {
+                const parent = attrs.append_to;
+                parent.appendChild(elem);
+            } else if (attr === 'innerHTML') {
+                elem.innerHTML = attrs.innerHTML;
+            } else {
+                elem.setAttribute(attr, attrs[attr]);
+            }
+        }
+        return elem;
+    }
+
+    function animateSVG(svgElement, attr, from, to) {
+        const animatedSvgElement = getAnimationElement(svgElement, attr, from, to);
+
+        if (animatedSvgElement === svgElement) {
+            // triggered 2nd time programmatically
+            // trigger artificial click event
+            const event = document.createEvent('HTMLEvents');
+            event.initEvent('click', true, true);
+            event.eventName = 'click';
+            animatedSvgElement.dispatchEvent(event);
+        }
+    }
+
+    function getAnimationElement(
+        svgElement,
+        attr,
+        from,
+        to,
+        dur = '0.4s',
+        begin = '0.1s'
+    ) {
+        const animEl = svgElement.querySelector('animate');
+        if (animEl) {
+            $.attr(animEl, {
+                attributeName: attr,
+                from,
+                to,
+                dur,
+                begin: 'click + ' + begin, // artificial click
+            });
+            return svgElement;
+        }
+
+        const animateElement = createSVG('animate', {
+            attributeName: attr,
+            from,
+            to,
+            dur,
+            begin,
+            calcMode: 'spline',
+            values: from + ';' + to,
+            keyTimes: '0; 1',
+            keySplines: cubic_bezier('ease-out'),
+        });
+        svgElement.appendChild(animateElement);
+
+        return svgElement;
+    }
+
+    function cubic_bezier(name) {
+        return {
+            ease: '.25 .1 .25 1',
+            linear: '0 0 1 1',
+            'ease-in': '.42 0 1 1',
+            'ease-out': '0 0 .58 1',
+            'ease-in-out': '.42 0 .58 1',
+        }[name];
+    }
+
+    $.on = (element, event, selector, callback) => {
+        if (!callback) {
+            callback = selector;
+            $.bind(element, event, callback);
+        } else {
+            $.delegate(element, event, selector, callback);
+        }
+    };
+
+    $.off = (element, event, handler) => {
+        element.removeEventListener(event, handler);
+    };
+
+    $.bind = (element, event, callback) => {
+        event.split(/\s+/).forEach(function (event) {
+            element.addEventListener(event, callback);
+        });
+    };
+
+    $.delegate = (element, event, selector, callback) => {
+        element.addEventListener(event, function (e) {
+            const delegatedTarget = e.target.closest(selector);
+            if (delegatedTarget) {
+                e.delegatedTarget = delegatedTarget;
+                callback.call(this, e, delegatedTarget);
+            }
+        });
+    };
+
+    $.closest = (selector, element) => {
+        if (!element) return null;
+
+        if (element.matches(selector)) {
+            return element;
+        }
+
+        return $.closest(selector, element.parentNode);
+    };
+
+    $.attr = (element, attr, value) => {
+        if (!value && typeof attr === 'string') {
+            return element.getAttribute(attr);
+        }
+
+        if (typeof attr === 'object') {
+            for (let key in attr) {
+                $.attr(element, key, attr[key]);
+            }
+            return;
+        }
+
+        element.setAttribute(attr, value);
+    };
+
+    class Arrow {
+        constructor(gantt, from_task, to_task) {
+            this.gantt = gantt;
+            this.from_task = from_task;
+            this.to_task = to_task;
+
+            this.calculate_path();
+            this.draw();
+        }
+
+        calculate_path() {
+            let start_x =
+                this.from_task.$bar.getX() + this.from_task.$bar.getWidth() / 2;
+
+            const condition = () =>
+                this.to_task.$bar.getX() < start_x + this.gantt.options.padding &&
+                start_x > this.from_task.$bar.getX() + this.gantt.options.padding;
+
+            while (condition()) {
+                start_x -= 10;
+            }
+
+            const start_y =
+                this.gantt.options.header_height +
+                this.gantt.options.bar_height +
+                (this.gantt.options.padding + this.gantt.options.bar_height) *
+                    this.from_task.task._index +
+                this.gantt.options.padding;
+
+            const end_x = this.to_task.$bar.getX() - this.gantt.options.padding / 2;
+            const end_y =
+                this.gantt.options.header_height +
+                this.gantt.options.bar_height / 2 +
+                (this.gantt.options.padding + this.gantt.options.bar_height) *
+                    this.to_task.task._index +
+                this.gantt.options.padding;
+
+            const from_is_below_to =
+                this.from_task.task._index > this.to_task.task._index;
+            const curve = this.gantt.options.arrow_curve;
+            const clockwise = from_is_below_to ? 1 : 0;
+            const curve_y = from_is_below_to ? -curve : curve;
+            const offset = from_is_below_to
+                ? end_y + this.gantt.options.arrow_curve
+                : end_y - this.gantt.options.arrow_curve;
+
+            this.path = `
+            M ${start_x} ${start_y}
+            V ${offset}
+            a ${curve} ${curve} 0 0 ${clockwise} ${curve} ${curve_y}
+            L ${end_x} ${end_y}
+            m -5 -5
+            l 5 5
+            l -5 5`;
+
+            if (
+                this.to_task.$bar.getX() <
+                this.from_task.$bar.getX() + this.gantt.options.padding
+            ) {
+                const down_1 = this.gantt.options.padding / 2 - curve;
+                const down_2 =
+                    this.to_task.$bar.getY() +
+                    this.to_task.$bar.getHeight() / 2 -
+                    curve_y;
+                const left = this.to_task.$bar.getX() - this.gantt.options.padding;
+
+                this.path = `
+                M ${start_x} ${start_y}
+                v ${down_1}
+                a ${curve} ${curve} 0 0 1 -${curve} ${curve}
+                H ${left}
+                a ${curve} ${curve} 0 0 ${clockwise} -${curve} ${curve_y}
+                V ${down_2}
+                a ${curve} ${curve} 0 0 ${clockwise} ${curve} ${curve_y}
+                L ${end_x} ${end_y}
+                m -5 -5
+                l 5 5
+                l -5 5`;
+            }
+        }
+
+        draw() {
+            this.element = createSVG('path', {
+                d: this.path,
+                'data-from': this.from_task.task.id,
+                'data-to': this.to_task.task.id,
+            });
+        }
+
+        update() {
+            this.calculate_path();
+            this.element.setAttribute('d', this.path);
+        }
+    }
+
     const YEAR = 'year';
     const MONTH = 'month';
     const DAY = 'day';
@@ -37,6 +266,20 @@ var Gantt = (function () {
             'Octubre',
             'Noviembre',
             'Diciembre',
+        ],
+        it: [
+            'Gennaio',
+            'Febbraio',
+            'Marzo',
+            'Aprile',
+            'Maggio',
+            'Giugno',
+            'Luglio',
+            'Agosto',
+            'Settembre',
+            'Ottobre',
+            'Novembre',
+            'Dicembre',
         ],
         ru: [
             'Январь',
@@ -107,6 +350,34 @@ var Gantt = (function () {
             '十月',
             '十一月',
             '十二月',
+        ],
+        de: [
+            'Januar',
+            'Februar',
+            'März',
+            'April',
+            'Mai',
+            'Juni',
+            'Juli',
+            'August',
+            'September',
+            'Oktober',
+            'November',
+            'Dezember',
+        ],
+        hu: [
+            'Január',
+            'Február',
+            'Március',
+            'Április',
+            'Május',
+            'Június',
+            'Július',
+            'Augusztus',
+            'Szeptember',
+            'Október',
+            'November',
+            'December',
         ],
     };
 
@@ -326,140 +597,6 @@ var Gantt = (function () {
             return padString.slice(0, targetLength) + String(str);
         }
     }
-
-    function $(expr, con) {
-        return typeof expr === 'string'
-            ? (con || document).querySelector(expr)
-            : expr || null;
-    }
-
-    function createSVG(tag, attrs) {
-        const elem = document.createElementNS('http://www.w3.org/2000/svg', tag);
-        for (let attr in attrs) {
-            if (attr === 'append_to') {
-                const parent = attrs.append_to;
-                parent.appendChild(elem);
-            } else if (attr === 'innerHTML') {
-                elem.innerHTML = attrs.innerHTML;
-            } else {
-                elem.setAttribute(attr, attrs[attr]);
-            }
-        }
-        return elem;
-    }
-
-    function animateSVG(svgElement, attr, from, to) {
-        const animatedSvgElement = getAnimationElement(svgElement, attr, from, to);
-
-        if (animatedSvgElement === svgElement) {
-            // triggered 2nd time programmatically
-            // trigger artificial click event
-            const event = document.createEvent('HTMLEvents');
-            event.initEvent('click', true, true);
-            event.eventName = 'click';
-            animatedSvgElement.dispatchEvent(event);
-        }
-    }
-
-    function getAnimationElement(
-        svgElement,
-        attr,
-        from,
-        to,
-        dur = '0.4s',
-        begin = '0.1s'
-    ) {
-        const animEl = svgElement.querySelector('animate');
-        if (animEl) {
-            $.attr(animEl, {
-                attributeName: attr,
-                from,
-                to,
-                dur,
-                begin: 'click + ' + begin, // artificial click
-            });
-            return svgElement;
-        }
-
-        const animateElement = createSVG('animate', {
-            attributeName: attr,
-            from,
-            to,
-            dur,
-            begin,
-            calcMode: 'spline',
-            values: from + ';' + to,
-            keyTimes: '0; 1',
-            keySplines: cubic_bezier('ease-out'),
-        });
-        svgElement.appendChild(animateElement);
-
-        return svgElement;
-    }
-
-    function cubic_bezier(name) {
-        return {
-            ease: '.25 .1 .25 1',
-            linear: '0 0 1 1',
-            'ease-in': '.42 0 1 1',
-            'ease-out': '0 0 .58 1',
-            'ease-in-out': '.42 0 .58 1',
-        }[name];
-    }
-
-    $.on = (element, event, selector, callback) => {
-        if (!callback) {
-            callback = selector;
-            $.bind(element, event, callback);
-        } else {
-            $.delegate(element, event, selector, callback);
-        }
-    };
-
-    $.off = (element, event, handler) => {
-        element.removeEventListener(event, handler);
-    };
-
-    $.bind = (element, event, callback) => {
-        event.split(/\s+/).forEach(function (event) {
-            element.addEventListener(event, callback);
-        });
-    };
-
-    $.delegate = (element, event, selector, callback) => {
-        element.addEventListener(event, function (e) {
-            const delegatedTarget = e.target.closest(selector);
-            if (delegatedTarget) {
-                e.delegatedTarget = delegatedTarget;
-                callback.call(this, e, delegatedTarget);
-            }
-        });
-    };
-
-    $.closest = (selector, element) => {
-        if (!element) return null;
-
-        if (element.matches(selector)) {
-            return element;
-        }
-
-        return $.closest(selector, element.parentNode);
-    };
-
-    $.attr = (element, attr, value) => {
-        if (!value && typeof attr === 'string') {
-            return element.getAttribute(attr);
-        }
-
-        if (typeof attr === 'object') {
-            for (let key in attr) {
-                $.attr(element, key, attr[key]);
-            }
-            return;
-        }
-
-        element.setAttribute(attr, value);
-    };
 
     class Bar {
         constructor(gantt, task) {
@@ -828,6 +965,7 @@ var Gantt = (function () {
         }
 
         update_progressbar_position() {
+            if (this.invalid) return;
             this.$bar_progress.setAttribute('x', this.$bar.getX());
             this.$bar_progress.setAttribute(
                 'width',
@@ -849,6 +987,7 @@ var Gantt = (function () {
         }
 
         update_handle_position() {
+            if (this.invalid) return;
             const bar = this.$bar;
             this.handle_group
                 .querySelector('.handle.left')
@@ -866,101 +1005,6 @@ var Gantt = (function () {
             for (let arrow of this.arrows) {
                 arrow.update();
             }
-        }
-    }
-
-    class Arrow {
-        constructor(gantt, from_task, to_task) {
-            this.gantt = gantt;
-            this.from_task = from_task;
-            this.to_task = to_task;
-
-            this.calculate_path();
-            this.draw();
-        }
-
-        calculate_path() {
-            let start_x =
-                this.from_task.$bar.getX() + this.from_task.$bar.getWidth() / 2;
-
-            const condition = () =>
-                this.to_task.$bar.getX() < start_x + this.gantt.options.padding &&
-                start_x > this.from_task.$bar.getX() + this.gantt.options.padding;
-
-            while (condition()) {
-                start_x -= 10;
-            }
-
-            const start_y =
-                this.gantt.options.header_height +
-                this.gantt.options.bar_height +
-                (this.gantt.options.padding + this.gantt.options.bar_height) *
-                    this.from_task.task._index +
-                this.gantt.options.padding;
-
-            const end_x = this.to_task.$bar.getX() - this.gantt.options.padding / 2;
-            const end_y =
-                this.gantt.options.header_height +
-                this.gantt.options.bar_height / 2 +
-                (this.gantt.options.padding + this.gantt.options.bar_height) *
-                    this.to_task.task._index +
-                this.gantt.options.padding;
-
-            const from_is_below_to =
-                this.from_task.task._index > this.to_task.task._index;
-            const curve = this.gantt.options.arrow_curve;
-            const clockwise = from_is_below_to ? 1 : 0;
-            const curve_y = from_is_below_to ? -curve : curve;
-            const offset = from_is_below_to
-                ? end_y + this.gantt.options.arrow_curve
-                : end_y - this.gantt.options.arrow_curve;
-
-            this.path = `
-            M ${start_x} ${start_y}
-            V ${offset}
-            a ${curve} ${curve} 0 0 ${clockwise} ${curve} ${curve_y}
-            L ${end_x} ${end_y}
-            m -5 -5
-            l 5 5
-            l -5 5`;
-
-            if (
-                this.to_task.$bar.getX() <
-                this.from_task.$bar.getX() + this.gantt.options.padding
-            ) {
-                const down_1 = this.gantt.options.padding / 2 - curve;
-                const down_2 =
-                    this.to_task.$bar.getY() +
-                    this.to_task.$bar.getHeight() / 2 -
-                    curve_y;
-                const left = this.to_task.$bar.getX() - this.gantt.options.padding;
-
-                this.path = `
-                M ${start_x} ${start_y}
-                v ${down_1}
-                a ${curve} ${curve} 0 0 1 -${curve} ${curve}
-                H ${left}
-                a ${curve} ${curve} 0 0 ${clockwise} -${curve} ${curve_y}
-                V ${down_2}
-                a ${curve} ${curve} 0 0 ${clockwise} ${curve} ${curve_y}
-                L ${end_x} ${end_y}
-                m -5 -5
-                l 5 5
-                l -5 5`;
-            }
-        }
-
-        draw() {
-            this.element = createSVG('path', {
-                d: this.path,
-                'data-from': this.from_task.task.id,
-                'data-to': this.to_task.task.id,
-            });
-        }
-
-        update() {
-            this.calculate_path();
-            this.element.setAttribute('d', this.path);
         }
     }
 
@@ -1133,6 +1177,10 @@ var Gantt = (function () {
 
                 // cache index
                 task._index = i;
+
+                if (typeof task.row_id === 'number') {
+                    task._index = task.row_id;
+                }
 
                 // invalid dates
                 if (!task.start && !task.end) {
@@ -1353,6 +1401,10 @@ var Gantt = (function () {
         }
 
         make_grid_rows() {
+            const distinctRows = [...new Set(this.tasks.map(x => x.row_id))];
+            for (let row of distinctRows){
+            }
+
             const rows_layer = createSVG('g', { append_to: this.layers.grid });
             const lines_layer = createSVG('g', { append_to: this.layers.grid });
 
@@ -1361,7 +1413,7 @@ var Gantt = (function () {
 
             let row_y = this.options.header_height + this.options.padding / 2;
 
-            for (let task of this.tasks) {
+            for (let row of distinctRows) {
                 createSVG('rect', {
                     x: 0,
                     y: row_y,
@@ -1419,10 +1471,7 @@ var Gantt = (function () {
                     tick_class += ' thick';
                 }
                 // thick ticks for quarters
-                if (
-                    this.view_is(VIEW_MODE.MONTH) &&
-                    (date.getMonth() + 1) % 3 === 0
-                ) {
+                if (this.view_is(VIEW_MODE.MONTH) && date.getMonth() % 3 === 0) {
                     tick_class += ' thick';
                 }
 
