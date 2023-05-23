f80cd37c8ec627d724f0ef09385ad6970f080a5f
Devjyoti Ghosh <devjyotighosh02@gmail.com>
2023-05-23 17:55:10 +0530

    Updated to add inline rows

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import date_utils from './date_utils';
-import { $, createSVG } from './svg_utils';
-import Bar from './bar';
 import Arrow from './arrow';
+import Bar from './bar';
+import date_utils from './date_utils';
 import Popup from './popup';
+import { $, createSVG } from './svg_utils';
 
 import './gantt.scss';
 
@@ -105,6 +105,10 @@ export default class Gantt {
 
             // cache index
             task._index = i;
+
+            if (typeof task.row_id === 'number') {
+                task._index = task.row_id;
+            }
 
             // invalid dates
             if (!task.start && !task.end) {
@@ -325,6 +329,12 @@ export default class Gantt {
     }
 
     make_grid_rows() {
+        let counter_rows = 0;
+        const distinctRows = [...new Set(this.tasks.map(x => x.row_id))];
+        for (let row of distinctRows){
+            counter_rows = counter_rows + 1;
+        }
+
         const rows_layer = createSVG('g', { append_to: this.layers.grid });
         const lines_layer = createSVG('g', { append_to: this.layers.grid });
 
@@ -333,7 +343,7 @@ export default class Gantt {
 
         let row_y = this.options.header_height + this.options.padding / 2;
 
-        for (let task of this.tasks) {
+        for (let row of distinctRows) {
             createSVG('rect', {
                 x: 0,
                 y: row_y,
